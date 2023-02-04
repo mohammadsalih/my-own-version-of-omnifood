e8ae5efb0995acb680598916edbb8a9e48bde238
mohammadsalih <mohammadsalih.main@gmail.com>
2023-02-04 15:21:54 +0300

    js added , but optimizing is still remains

--- js/script.js
+++ js/script.js
@@ -13,7 +13,39 @@ function open_nav() {
   }
   nav[0].classList.toggle("open-nav");
 }
+// this function is to remove the navigation when the user click on one of the links
+function close_nav() {
+  const all_nav_links = document.querySelectorAll(".main-nav-link");
+
+  all_nav_links.forEach(function (link) {
+    link.addEventListener("click", open_nav);
+  });
+}
+// this funcion is to make the navigatation stick to the top
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 // /////////////////////
 // calling the functions
 set_year();
+close_nav();
